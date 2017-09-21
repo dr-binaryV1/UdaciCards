f9922016fcdc99c2f0c7efaefaa800bee3d9f20d
dr-binaryV1 <developerdamian@gmail.com>
2017-09-20 21:48:43 -0500

    chore: Initialize DeckView component

---
+++ components/DeckView.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, StyleSheet, TouchableOpacity, View } from 'react-native';
+
+export default function DeckView () {
+  return (
+    <View>Deck View</View>
+  )
+}
