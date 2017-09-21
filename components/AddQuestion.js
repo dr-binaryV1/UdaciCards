0a0c4f636e3ee2e823eff6df7681ac457ff0b21d
damian.wynter <damian.wynter@particularpresence.com>
2017-09-21 16:06:21 -0500

    feat: Add Question component init

---
+++ components/AddQuestion.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Text, KeyboardAvoidingView, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+export default class AddQuestion extends Component {
+  state = {
+    question: '',
+    answer: ''
+  }
+
+  render() {
+    const { container, inputStyle, addCardButton } = styles;
+
+    return (
+      <KeyboardAvoidingView behavior='padding' style={container}>
+        <MaterialCommunityIcons name='cards' size={300} color='#1485ff' />
+        <TextInput
+          style={inputStyle}
+          placeholder='Enter Question'
+          onChangeText={text => this.setState({ question: text })}
+          value={this.state.question}
+        />
+
+        <TextInput
+          style={inputStyle}
+          placeholder='Enter Answer'
+          multiline={true}
+          onChangeText={text => this.setState({ answer: text })}
+          value={this.state.answer}
+        />
+
+        <TouchableOpacity style={addCardButton}>
+          <Text style={{ fontSize: 20, color: '#FFF' }}>Submit Card</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  inputStyle: {
+    width: 500,
+    height: 80,
+    fontSize: 30
+  },
+  addCardButton: {
+    width: 200,
+    backgroundColor: '#1485ff',
+    borderWidth: 1,
+    borderColor: '#FFF',
+    borderRadius: 5,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30
+  }
+})
