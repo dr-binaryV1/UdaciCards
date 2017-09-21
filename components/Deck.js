052bb0d0e4bf0af20f97bb6154aad3ca4f67ede7
damian.wynter <damian.wynter@particularpresence.com>
2017-09-21 16:06:52 -0500

    fix: Add onPress event to route to DeckView

--- components/Deck.js
+++ components/Deck.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 
+import { getDeck } from '../utils/helpers';
+
 export default function Deck(props) {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity
+      onPress={() => /*getDeck(props.title).then(result => console.log(result))*/ 
+        props.navigation.navigate(
+          'DeckView',
+          {title: props.title}
+      )}>
       <View style={styles.containerStyle}>
         <Text style={styles.titleStyle}>{props.title}</Text>
         <Text style={styles.cardNumberDesc}>{`${props.cardNumber} card(s)`}</Text>
@@ -21,7 +28,14 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 5,
     marginBottom: 20,
-    height: 150
+    height: 150,
+    shadowRadius: 6,
+    shadowOpacity: 1,
+    shadowColor: 'rgba(0,0,0,24)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    }
   },
   titleStyle: {
     fontSize: 30
