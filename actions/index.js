21b3b3196a8d521280acbb7cabe5d758a107885d
damian.wynter <damian.wynter@particularpresence.com>
2017-09-22 13:30:13 -0500

    feat: Create fetchDecks, sendDeckTitle, sendCardToDeck actions

--- actions/index.js
+++ actions/index.js
@@ -10,11 +10,32 @@ export const GET_DECK = 'GET_DECK';
 export const ADD_CARD_TO_DECK = 'ADD_CARD_TO_DECK';
 export const SAVE_DECK_TITLE = 'SAVE_DECK_TITLE';
 
-export function fetchDecks() {
+const receiveDecks = decks => ({
+  type: GET_DECKS,
+  decks
+});
+
+const savedDeckTitle = decks => ({
+  type: SAVE_DECK_TITLE,
+  decks
+})
+
+export const fetchDecks = () => dispatch => {
   getDecks().then((decks) => {
-    return {
-      type: GET_DECKS,
-      decks
-    }
+    dispatch(receiveDecks(JSON.parse(decks)));
+  }).catch(err => console.log('Error: ', err));
+}
+
+export const sendDeckTitle = (title) => dispatch => {
+  saveDeckTitle(title).then(() => {
+    getDecks().then((decks) => {
+      dispatch(savedDeckTitle(JSON.parse(decks)));
+    }).catch(err => console.log('Error ', err));
+  })
+}
+
+export const sendCardToDeck = (title, card) => dispatch => {
+  addCardToDeck(title, card).then(() => {
+    fetchDecks()(dispatch);
   })
 }
