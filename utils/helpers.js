ef5655b5b5dfac7f7ceaa464e83f6320733b0ac9
damian.wynter <damian.wynter@particularpresence.com>
2017-09-26 11:47:18 -0500

    feat: Add Notification helper methods

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,8 @@
-import { AsyncStorage } from 'react-native';
-const CARD_STORAGE_KEY = 'flashCards:key';
+import { AsyncStorage } from 'react-native';;
+import { Notifications, Permissions } from 'expo';
+
+const NOTIFICATION_KEY = 'flashCards:notification';
+const CARD_STORAGE_KEY = 'flashCards:key'
 
 export function getDecks() {
   return AsyncStorage.getItem(CARD_STORAGE_KEY, (err, result) => {
@@ -30,5 +33,55 @@ export function addCardToDeck(title, card) {
       const data = JSON.parse(result);
       data[title].questions.push(card);
       AsyncStorage.setItem(CARD_STORAGE_KEY, JSON.stringify(data));
+    })
+}
+
+export function clearLocalNotification () {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY)
+    .then(Notifications.cancelAllScheduledNotificationsAsync());
+}
+
+export function createNotification () {
+  return {
+    title: 'Remember to Study',
+    body: "Don't forget to study your flash cards today",
+    ios: {
+      sound: true
+    },
+    android: {
+      sound: true,
+      priority: 'high',
+      sticky: false,
+      vibrate: true
+    }
+  }
+}
+
+export function setLocalNotification () {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then((data) => {
+      if(data === null || data === false) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS)
+          .then(({ status }) => {
+            if (status === 'granted') {
+              Notifications.cancelAllScheduledNotificationsAsync();
+
+              let tomorrow = new Date();
+              tomorrow.setDate(tomorrow.getDate() + 1);
+              tomorrow.setHours(8);
+              tomorrow.setMinutes(0);
+
+              Notifications.scheduleLocalNotificationAsync(
+                createNotification(),
+                {
+                  time: tomorrow,
+                  repeat: 'day'
+                }
+              );
+              AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true));
+            }
+          })
+      }
     })
 }
